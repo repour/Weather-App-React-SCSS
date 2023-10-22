8eaa37e4fbe4fcc4dc4cb61a2822aa855ce99e33
repour <poorsadeghi.r@gmail.com>
2023-10-22 17:41:56 +0330

    final

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f45218e77emsh80eecf74552ebfep160da0jsn7b9647eea20f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "0c920c5ffaf0cc40fe514c3798a0b297"
---
+++ src/components/search/search.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import PropTypes from 'prop-types'
+import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions } from "../../api"
+
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`
+                        }
+                    })
+                }
+            })
+            .catch(error => console.error(error))
+    };
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData)
+    };
+
+
+    return (
+        <>
+            <AsyncPaginate
+                placeholder='Serach for city'
+                debounceTimeout={600}
+                value={search}
+                onChange={handleOnChange}
+                loadOptions={loadOptions}
+            />
+
+        </>
+    )
+}
+
+
+Search.propTypes = {
+    onSearchChange: PropTypes.func.isRequired,
+}
+
+
+export default Search
